fb11abd77be4dcbb91a3f4c7661cceb111385a4b
xmin3 <Xmin.34@gmail.com>
2022-09-03 11:24:17 +0300

    Commit HW.1

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задача №1");
         int year = 2027;
-        printYear(year);
+        whatYear(year);
         System.out.println("Задача №2");
         int clientOs = 0;
         int clientDeviceYear = 2013;
-        clientOS(clientOs,clientDeviceYear);
+        findClientOS(clientOs, clientDeviceYear);
         System.out.println("Задача №3");
-        int deliveryDistance = 195;
-        dleviryDays(deliveryDistance);
+        howLong();
     }
 
-
-    public static void printYear(int year) {
-        if (year % 100 == 0 && year % 400 == 0)
+    private static void whatYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.printf("%s - високосный год\n", year);
-        else if (year % 4 == 0 && year % 100 > 0)
-            System.out.printf("%s - високосный год\n", year);
-        else if (year % 100 == 0)
+        } else {
             System.out.printf("%s - не високосный год\n", year);
-        else
-            System.out.printf("%s - не високосный год\n", year);
+        }
     }
 
-    public static void clientOS(int clientOs , int clientDeviceYear) {
-                if (clientOs == 1) {
-            if (clientDeviceYear > 2015)
+    private static void findClientOS(int clientOs, int clientDeviceYear) {
+        if (clientOs == 1) {
+            if (clientDeviceYear > 2015) {
                 System.out.println("Установите версию приложения для IOS по ссылке");
-            else System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }
         }
         if (clientOs == 0) {
-            if (clientDeviceYear > 2015)
+            if (clientDeviceYear > 2015) {
                 System.out.println("Установите версию приложения для Android по ссылке");
-            else System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
     }
 
-    public static void dleviryDays(int deliveryDistance) {
+    private static int howLong() {
+        int deliveryDistance = 95;
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: 1 день");
         }
@@ -49,6 +49,7 @@ public class Main {
         } else {
             System.out.println("Вне зоны доставки");
         }
+        return deliveryDistance;
     }
 }
 
